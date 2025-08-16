587494a9fbfd937a2c265761bcb3c6fcecd5e74b
ved727272 <brijesh.zte@gmail.com>
2025-08-16 19:51:04 +0530

    rgandstgad

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name = "sandeep"
   location = "eastus"
   }
+
+  resource "azurerm_resource_group" "rg1" {
+  name = "sandeep1"
+  location = "eastus"
+  }
